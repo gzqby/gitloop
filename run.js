b244c0d985b5f15ed0be28b6fafa9595c01b5627
zigo <zguoby@gmail.com>
2020-11-24 12:22:31 +0800

    bug-free

--- run.js
+++ run.js
@@ -5,12 +5,12 @@ const path = require('path');
 const gitloop = require("./index.js");
 
 const argvs = process.argv;
-const GITLOOPINDEX = process.argv0 === "gitloop" ? 1 : 2;
+const GITLOOPINDEX = 2;
 const _onlyIndexOf = (argvs.indexOf('-o') || argvs.indexOf('--only'));
 const _only = _onlyIndexOf===-1?false:true;
-const folder = argvs[_only?_only+1:GITLOOPINDEX];
-const _command = _only ? argvs.slice(_onlyIndexOf) : argvs.slice(GITLOOPINDEX+1);
+const folder = argvs[_only?GITLOOPINDEX+1:GITLOOPINDEX];
+const _command = _only ? argvs.slice(GITLOOPINDEX) : argvs.slice(GITLOOPINDEX+1);
 
-const resolveFolderUrl = path.resolve(__dirname, folder);
+const resolveFolderUrl = path.resolve('./', folder);
 
 gitloop(resolveFolderUrl, {_only, _command: _command.join(" ")});
